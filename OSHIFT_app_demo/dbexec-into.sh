d9ca9c67ef8d50b6c9efba146be8973082bb8e88
Jody Hunt <jody.hunt@cyberark.com>
2019-07-19 22:21:13 -0500

    setup for demo shell

--- OSHIFT_app_demo/dbexec-into.sh
+++ OSHIFT_app_demo/dbexec-into.sh
@@ -1,14 +1,7 @@
 #!/bin/bash 
 . utils.sh
 # finds pod matching pod_name_substr and follows the secretless container log
-if [[ $1 == pg ]]; then
-  pod_name_substr=postgres-db
-elif [[ $1 == ms ]]; then
-  pod_name_substr=mysql-db
-else
-  echo "Usage: $0 [ pg | ms ]"
-  exit -1
-fi
+pod_name_substr=postgres-db
 
 set_namespace $TEST_APP_NAMESPACE_NAME
 app_pod=$($cli get pods | grep "Running" | grep $pod_name_substr | awk '{print $1}')
