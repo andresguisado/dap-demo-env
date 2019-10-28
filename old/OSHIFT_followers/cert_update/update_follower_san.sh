9bbb2a67cc98e1404807e6c9fe400a23fc3aa3fc
Jody Hunt <jody.hunt@cyberark.com>
2019-10-28 10:39:21 -0500

    retool for non-modal env config

--- old/OSHIFT_followers/cert_update/update_follower_san.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -x
-
-FOLLOWER_ALTNAMES=$FOLLOWER_ALTNAMES,conjur-follower-cyberark.192.168.99.100.nip.io
-docker exec $CONJUR_MASTER_CONTAINER_NAME evoke ca issue --force conjur-follower $FOLLOWER_ALTNAMES
-docker exec $CONJUR_MASTER_CONTAINER_NAME evoke seed follower conjur-follower > $FOLLOWER_SEED_FILE
